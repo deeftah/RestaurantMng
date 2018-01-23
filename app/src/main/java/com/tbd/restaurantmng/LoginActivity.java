bed4bc198812c6d11cb728757e3be2668480edc1
tthrash <hp6449yq@metrostate.edu>
2018-01-23 17:58:15 -0600

    Mostly changes to Android code.

--- app/src/main/java/com/tbd/restaurantmng/LoginActivity.java
+++ app/src/main/java/com/tbd/restaurantmng/LoginActivity.java
@@ -2,6 +2,8 @@ package com.tbd.restaurantmng;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // login functionality
+        Button loginB = (Button) findViewById(R.id.loginB);
+
+        loginB.setOnClickListener(
+                new Button.OnClickListener() {
+                     public void onClick(View v) {
+
+                     }
+                }
+        );
+
+        // create account functionality
+        Button createAccB = (Button) findViewById(R.id.createAccB);
+
+        createAccB.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
     }
 }
